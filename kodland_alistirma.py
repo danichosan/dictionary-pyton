6eddc3e100cd32870aaa437d62f65127ab47f8a7
danichosan <130922635+danichosan@users.noreply.github.com>
2023-04-16 20:24:23 +0300

    Create kodland_alistirma.py

---
+++ kodland_alistirma.py
@@ -0,0 +1,20 @@
+meme_sozlugu = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "GG": "good game yani iyi oyundu",
+            "CREEPY": "urkutucu",
+            "SHEESH": "onaylamamak"
+            
+            }
+            
+            
+kelime = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if kelime in meme_sozlugu.keys():
+    print("Kelime sözlukte var")
+    
+else:
+    print("Kelime sözlukte yok :(")
+            
+            
+            
